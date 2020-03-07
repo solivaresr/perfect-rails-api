8b5975d642c9340cd3f333e99fc7cc3e35f5ea10
solivaresr <kolivaresrojas@gmail.com>
2020-03-07 18:40:43 -0300

    configuro CORS

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,16 @@ module PerfectRailsApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    # Config CORS
+    # If you’re building a public API you’ll probably want to enable Cross-Origin Resource Sharing (CORS),
+    # in order to make cross-origin AJAX requests possible.
+    config.middleware.insert_before 0, 'Rack::Cors' do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: %i[get post options]
+      end
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
